c78e28454ac2f6245af9aac148959098cae44f50
sanghyeup.lee <l.sanghyeup.gmail>
2022-05-28 16:48:36 +0900

    Feat : 초기파일 추가됨

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# install docker
+if [ ! -x "$(command -v docker)" ]; then
+    echo 'installing docker..'
+    sudo apt update
+    sudo apt-get install -y ca-certificates curl software-properties-common apt-transport-https gnupg lsb-release
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+    sudo apt update
+    sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+fi
+
+# install docker-compose
+if [ ! -e /usr/local/bin/docker-compose ]; then
+    sudo curl -L https://github.com/docker/compose/releases/download/v2.1.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
