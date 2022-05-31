7312a9e7145cf50bb755e4e4d977371c431f8e8b
sanghyeup.lee <l.sanghyeup.gmail>
2022-06-01 07:34:56 +0900

    Feat : ghost 구현중 임시저장

--- install.sh
+++ install.sh
@@ -20,6 +20,9 @@ fi
 
 # install docker-compose
 if [ ! -e /usr/local/bin/docker-compose ]; then
+    echo 'installing docker-compose..'
     sudo curl -L https://github.com/docker/compose/releases/download/v2.1.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 fi
+
+docker-compose up
